ce3ab1207b19418c9194be62069a7d22c013f847
Javiera <javiera.guajardo.g@usach.cl>
2025-09-22 17:00:17 -0300

    codigo base back

---
+++ src/test/java/com/example/Lab1_tbd/Lab1TbdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Lab1_tbd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab1TbdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
