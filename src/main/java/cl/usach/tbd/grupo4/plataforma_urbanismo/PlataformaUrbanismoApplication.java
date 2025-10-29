3daa9f5feba1265783c3bc63f4bd87345f902130
Brandon Duarte Miranda <brandon.duarte@usach.cl>
2025-10-29 10:18:36 -0300

    Reemplazo total con la estructura base de Spring Boot

---
+++ src/main/java/cl/usach/tbd/grupo4/plataforma_urbanismo/PlataformaUrbanismoApplication.java
@@ -0,0 +1,13 @@
+package cl.usach.tbd.grupo4.plataforma_urbanismo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlataformaUrbanismoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlataformaUrbanismoApplication.class, args);
+	}
+
+}
